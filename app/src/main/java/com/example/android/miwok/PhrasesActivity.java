e225ba7743b6b736c54173dfefcac76421f4d508
Medha Gupta <medhagupta312002@gmail.com>
2021-04-26 20:32:02 +0530

    Miwok app

---
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class PhrasesActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener=
+            new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        mMediaPlayer.pause();
+                        mMediaPlayer.seekTo(0);
+
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        mMediaPlayer.start();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        releaseMediaPlayer();
+                    }
+                }
+            };
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Where are you going?","minto wuksus",R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?","tinnə oyaase'nə",R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...","oyaaset...",R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?","michəksəs?",R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I'm feeling good.","kuchi achit",R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?","əənəs'aa?",R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes,I'm coming.","həə'əənəm",R.raw.phrase_yes_im_coming));
+        words.add(new Word("I'm coming.","əənəm",R.raw.phrase_im_coming));
+        words.add(new Word("Let's go.","yoowutis",R.raw.phrase_lets_go));
+        words.add(new Word("Come here.","ənni'nem",R.raw.phrase_come_here));
+        WordAdapter adapter = new WordAdapter(this, words,R.color.category_phrases);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                releaseMediaPlayer();
+                Word word= words.get(position);
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getmAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer(){
+        if(mMediaPlayer!=null)
+            mMediaPlayer.release();
+        mMediaPlayer=null;
+        mAudioManager.abandonAudioFocus((mOnAudioFocusChangeListener));
+    }
+}
